b295191b1fee3501f438422cb451edfab9290c2b
Milica Bjelica <milicabjelica@Milicas-MacBook-Pro.local>
2026-05-04 12:30:11 +0200

    final fix

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 function saberi(a, b) {
-  return a - b;
+  return a + b;
 }
 
 module.exports = saberi;
--- test.js
+++ test.js
@@ -1,7 +1,7 @@
 const saberi = require("./index");
 
-if (saberi(2, 3) !== 6) {
-  throw new Error("Test nije prošao: 2 + 3 treba da bude 5");
+if (saberi(2, 3) !== 5) {
+  throw new Error("Test nije prošao");
 }
 
 console.log("Svi testovi su uspješno prošli.");
